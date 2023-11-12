525ffdbd26277c49c2178897f8504a9955a4d532
Jay Bothra <w0814392@myscc.ca>
2023-11-12 18:22:58 -0500

    Finished View Pager and Recycler View Styling in both is left

--- app/src/main/java/com/jaybothra/flutefusion/ViewPager/CustomViewPagerAdapter.java
+++ app/src/main/java/com/jaybothra/flutefusion/ViewPager/CustomViewPagerAdapter.java
@@ -16,15 +16,14 @@ public class CustomViewPagerAdapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        position = position % 7;
+        position = position % 6;
         switch (position){
-            case 0: return MaintainFragment.newInstance("", R.mipmap.ic_launcher, "", "", "");
-            case 1: return MaintainFragment.newInstance(" ", R.mipmap.ic_launcher, "", "", "");
-            case 2: return MaintainFragment.newInstance("   ", R.mipmap.ic_launcher, "", "", "");
-            case 3: return MaintainFragment.newInstance("  ", R.mipmap.ic_launcher, "", "", "");
-            case 4: return MaintainFragment.newInstance(" m", R.mipmap.ic_launcher, "", "", "");
-            case 5: return MaintainFragment.newInstance(" a", R.mipmap.ic_launcher, "", "", "");
-            case 6: return MaintainFragment.newInstance(" e", R.mipmap.ic_launcher, "", "", "");
+            case 0: return MaintainFragment.newInstance(MaintainConsts.VP_TITLE1, MaintainConsts.VP_IMG1, "1. "+MaintainConsts.VP_TEXT1_1, "2. "+MaintainConsts.VP_TEXT2_1, "3. "+MaintainConsts.VP_TEXT3_1);
+            case 1: return MaintainFragment.newInstance(MaintainConsts.VP_TITLE2, MaintainConsts.VP_IMG2, "1. "+MaintainConsts.VP_TEXT1_2, "2. "+MaintainConsts.VP_TEXT2_2, "3. "+MaintainConsts.VP_TEXT3_2);
+            case 2: return MaintainFragment.newInstance(MaintainConsts.VP_TITLE3, MaintainConsts.VP_IMG3, "1. "+MaintainConsts.VP_TEXT1_3, "2. "+MaintainConsts.VP_TEXT2_3, "3. "+MaintainConsts.VP_TEXT3_3);
+            case 3: return MaintainFragment.newInstance(MaintainConsts.VP_TITLE4, MaintainConsts.VP_IMG4, "1. "+MaintainConsts.VP_TEXT1_4, "2. "+MaintainConsts.VP_TEXT2_4, "3. "+MaintainConsts.VP_TEXT3_4);
+            case 4: return MaintainFragment.newInstance(MaintainConsts.VP_TITLE5, MaintainConsts.VP_IMG5, "1. "+MaintainConsts.VP_TEXT1_5, "2. "+MaintainConsts.VP_TEXT2_5, "3. "+MaintainConsts.VP_TEXT3_5);
+            case 5: return MaintainFragment.newInstance(MaintainConsts.VP_TITLE6, MaintainConsts.VP_IMG6, "1. "+MaintainConsts.VP_TEXT1_6, "2. "+MaintainConsts.VP_TEXT2_6, "3. "+MaintainConsts.VP_TEXT3_6);
             default: return MaintainFragment.newInstance("404 Error", R.mipmap.ic_launcher, "", "", "");
         }
     }
--- app/src/main/java/com/jaybothra/flutefusion/ViewPager/MaintainConsts.java
+++ app/src/main/java/com/jaybothra/flutefusion/ViewPager/MaintainConsts.java
@@ -7,49 +7,42 @@ public class MaintainConsts {
     public static final String VP_TEXT1_1 = "Use a soft cloth or a piece of cotton to gently wipe the exterior of the Bansuri. Remove any dust, moisture, or fingerprints.";
     public static final String VP_TEXT2_1 = "Take extra care around the finger holes and the embouchure area.";
     public static final String VP_TEXT3_1 = "Avoid using harsh chemicals or excessive force, as it may damage the bamboo.";
-    public static final int VP_IMG1 = R.mipmap.ic_launcher;
+    public static final int VP_IMG1 = R.drawable.vpimg1;
 
 
-    public static final String VP_TITLE2 = "";
-    public static final String VP_TEXT1_2 = "";
-    public static final String VP_TEXT2_2 = "";
-    public static final String VP_TEXT3_2 = "";
-    public static final int VP_IMG2 = R.mipmap.ic_launcher;
+    public static final String VP_TITLE2 = "Interior Cleaning";
+    public static final String VP_TEXT1_2 = "Insert a cleaning rod or a thin, soft brush through the embouchure hole to clean the interior of the Bansuri.";
+    public static final String VP_TEXT2_2 = "Move the rod in a circular motion to remove any accumulated dirt or moisture.";
+    public static final String VP_TEXT3_2 = "Ensure that the cleaning tool is soft to avoid scratches inside the bamboo.";
+    public static final int VP_IMG2 = R.drawable.vpimg2;
 
 
-    public static final String VP_TITLE3 = "";
-    public static final String VP_TEXT1_3 = "";
-    public static final String VP_TEXT2_3 = "";
-    public static final String VP_TEXT3_3 = "";
-    public static final int VP_IMG3 = R.mipmap.ic_launcher;
+    public static final String VP_TITLE3 = "Oil Application";
+    public static final String VP_TEXT1_3 = "Apply a small amount of bore oil to the exterior of the Bansuri using a soft cloth.";
+    public static final String VP_TEXT2_3 = "Gently rub the oil into the bamboo to moisturize and protect it from cracking.";
+    public static final String VP_TEXT3_3 = "Use specific flute or bore oil, and avoid excess oil to prevent clogging of the finger holes.";
+    public static final int VP_IMG3 = R.drawable.vpimg3;
 
 
-    public static final String VP_TITLE4 = "";
-    public static final String VP_TEXT1_4 = "";
-    public static final String VP_TEXT2_4 = "";
-    public static final String VP_TEXT3_4 = "";
-    public static final int VP_IMG4 = R.mipmap.ic_launcher;
+    public static final String VP_TITLE4 = "Checking for Cracks";
+    public static final String VP_TEXT1_4 = "Inspect the entire length of the Bansuri for any cracks or splits.";
+    public static final String VP_TEXT2_4 = "Pay special attention to the joints and areas near the finger holes.";
+    public static final String VP_TEXT3_4 = "If you find any cracks, consult with a professional flute maker or repair technician.";
+    public static final int VP_IMG4 = R.drawable.vpimg4;
 
 
-    public static final String VP_TITLE5 = "";
-    public static final String VP_TEXT1_5 = "";
-    public static final String VP_TEXT2_5 = "";
-    public static final String VP_TEXT3_5 = "";
-    public static final int VP_IMG5 = R.mipmap.ic_launcher;
+    public static final String VP_TITLE5 = "Finger Hole Cork Inspection";
+    public static final String VP_TEXT1_5 = "Examine the cork plugs in the finger holes for wear or damage.";
+    public static final String VP_TEXT2_5 = "If necessary, replace the cork to maintain proper sealing and tuning.";
+    public static final String VP_TEXT3_5 = "Cork replacement can be done by a skilled instrument repair technician.";
+    public static final int VP_IMG5 = R.drawable.vpimg5;
 
 
-    public static final String VP_TITLE6 = "";
-    public static final String VP_TEXT1_6 = "";
-    public static final String VP_TEXT2_6 = "";
-    public static final String VP_TEXT3_6 = "";
-    public static final int VP_IMG6 = R.mipmap.ic_launcher;
-
-
-    public static final String VP_TITLE7 = "";
-    public static final String VP_TEXT1_7 = "";
-    public static final String VP_TEXT2_7 = "";
-    public static final String VP_TEXT3_7 = "";
-    public static final int VP_IMG7 = R.mipmap.ic_launcher;
+    public static final String VP_TITLE6 = "Storage and Climate Control";
+    public static final String VP_TEXT1_6 = "Store the Bansuri in a protective case to shield it from dust, moisture, and temperature changes.";
+    public static final String VP_TEXT2_6 = "Avoid extreme temperatures and humidity, as they can adversely affect the bamboo.";
+    public static final String VP_TEXT3_6 = "Consider using a desiccant in the case to control moisture levels.";
+    public static final int VP_IMG6 = R.drawable.vpimg6;
 
 
 
