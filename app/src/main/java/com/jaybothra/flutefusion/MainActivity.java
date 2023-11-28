4d8ccf4635b6395c9aa61fe82d65fe7ac715e546
Jay Bothra <w0814392@myscc.ca>
2023-11-28 14:34:04 -0500

    Removed fab from all pages and fixed the header

--- app/src/main/java/com/jaybothra/flutefusion/MainActivity.java
+++ app/src/main/java/com/jaybothra/flutefusion/MainActivity.java
@@ -30,17 +30,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_welcome,
                 R.id.nav_store,
