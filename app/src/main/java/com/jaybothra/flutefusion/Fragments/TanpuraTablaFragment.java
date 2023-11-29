13828ca3caea82160a44f5c44fb9c4f5dad04ee5
Jay Bothra <w0814392@myscc.ca>
2023-11-28 21:27:07 -0500

    Designed Audio Page added seek bar, play pause button added some information of tanpura Audio Wroking Properly

--- app/src/main/java/com/jaybothra/flutefusion/Fragments/TanpuraTablaFragment.java
+++ app/src/main/java/com/jaybothra/flutefusion/Fragments/TanpuraTablaFragment.java
@@ -1,44 +1,37 @@
 package com.jaybothra.flutefusion.Fragments;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.media.MediaPlayer;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.jaybothra.flutefusion.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TanpuraTablaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TanpuraTablaFragment extends Fragment {
+import java.util.Locale;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+public class TanpuraTablaFragment extends Fragment implements SeekBar.OnSeekBarChangeListener {
+    private MediaPlayer mediaPlayer;
+    private ImageButton btnPlay, btnPause;
+    private SeekBar seekBar;
+    private boolean isSeeking = false;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private Handler handler;
+    private TextView timeRemainingTextView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public TanpuraTablaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TanpuraTablaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static TanpuraTablaFragment newInstance(String param1, String param2) {
         TanpuraTablaFragment fragment = new TanpuraTablaFragment();
         Bundle args = new Bundle();
@@ -49,18 +42,130 @@ public class TanpuraTablaFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tanpura_tabla, container, false);
+
+        mediaPlayer = MediaPlayer.create(getActivity(), R.raw.tanpura_a);
+
+        seekBar = view.findViewById(R.id.seekBar);
+        btnPlay = view.findViewById(R.id.btnPlay);
+        btnPause = view.findViewById(R.id.btnPause);
+        timeRemainingTextView = view.findViewById(R.id.timeline);
+        seekBar.setOnSeekBarChangeListener(this);
+        handler = new Handler();
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                    btnPlay.setVisibility(View.GONE);
+                    btnPause.setVisibility(View.VISIBLE);
+                    startSeekBarUpdate(); // Start updating SeekBar when audio starts
+                }
+            }
+        });
+
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    btnPlay.setVisibility(View.VISIBLE);
+                    btnPause.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // When audio playback is completed, reset UI
+                btnPlay.setVisibility(View.VISIBLE);
+                btnPause.setVisibility(View.GONE);
+            }
+        });
+
+        if (mediaPlayer != null) {
+            int totalDuration = mediaPlayer.getDuration();
+            int totalMinutes = (totalDuration / 1000) / 60;
+            int totalSeconds = (totalDuration / 1000) % 60;
+            String totalDurationStr = String.format(Locale.getDefault(), "%02d:%02d", totalMinutes, totalSeconds);
+            timeRemainingTextView.setText(totalDurationStr);
+        }
+
+        return view;
+    }
+    private void startSeekBarUpdate() {
+        if (mediaPlayer != null) {
+            seekBar.setMax(mediaPlayer.getDuration());
+
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                        int currentPosition = mediaPlayer.getCurrentPosition();
+                        int totalDuration = mediaPlayer.getDuration();
+
+                        // Calculate time remaining in milliseconds
+                        int timeRemaining = totalDuration - currentPosition;
+
+                        // Convert milliseconds to minutes and seconds
+                        int minutes = (timeRemaining / 1000) / 60;
+                        int seconds = (timeRemaining / 1000) % 60;
+
+                        // Format the time remaining string
+                        String timeRemainingStr = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+                        // Update the TextView with the time remaining
+                        timeRemainingTextView.setText(timeRemainingStr);
+
+                        // Update SeekBar progress
+                        seekBar.setProgress(currentPosition);
+                    }else {
+                        // If music is not playing, show the total audio length
+                        int totalDuration = mediaPlayer.getDuration();
+                        int totalMinutes = (totalDuration / 1000) / 60;
+                        int totalSeconds = (totalDuration / 1000) % 60;
+                        String totalDurationStr = String.format(Locale.getDefault(), "%02d:%02d", totalMinutes, totalSeconds);
+                        timeRemainingTextView.setText(totalDurationStr);
+                    }
+                    handler.postDelayed(this, 1000);
+                }
+            });
+        }
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Remove callbacks to prevent memory leaks
+        if (handler != null) {
+            handler.removeCallbacksAndMessages(null);
+        }
+    }
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (fromUser) {
+            mediaPlayer.seekTo(progress);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tanpura_tabla, container, false);
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        isSeeking = true;
     }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        isSeeking = false;
+    }
+
 }
+
+
